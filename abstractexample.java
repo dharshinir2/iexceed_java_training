b12809a966a9789fb35f1204ea231791d128cb95
DHARSHINI R <113581056+dharshinir2@users.noreply.github.com>
2025-05-08 21:52:28 +0530

    Add files via upload

---
+++ abstractexample.java
@@ -0,0 +1,23 @@
+abstract class Sample {
+    public void show() {
+        System.out.println("Hello");
+    }
+
+    abstract public void display(int x, int y);
+}
+
+class A extends Sample {
+    @Override
+    public void display(int x, int y) {
+        System.out.println(x + y);
+    }
+}
+
+public class Main {  
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.display(40, 10);  
+        obj.show();
+    }
+}
+
