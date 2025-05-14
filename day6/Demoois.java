b11b895426de419edceccf4e975ded843cfda3e9
DHARSHINI R <113581056+dharshinir2@users.noreply.github.com>
2025-05-14 13:09:37 +0530

    Add files via upload

---
+++ day6/Demoois.java
@@ -0,0 +1,31 @@
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.*;
+class student implements Serializable{
+	String name;
+	int id;
+	student(String name,int id)
+	{
+		this.name=name;
+		this.id=id;
+	}	
+}
+class Demoois{
+	public static void main(String[] args) throws Exception
+	{
+		student s1=new student("Dharshini",17);
+		FileOutputStream fo=new FileOutputStream("sample.txt");
+		ObjectOutputStream o=new ObjectOutputStream(fo);
+		o.writeObject(s1);
+		o.close();
+		
+		
+		FileInputStream fi=new FileInputStream("sample.txt");
+		ObjectInputStream oi=new ObjectInputStream(fi); //deseriallization
+		student si=(student)oi.readObject();
+		System.out.println("Name "+ si.name);
+		
+		
+		
+	}
+}
