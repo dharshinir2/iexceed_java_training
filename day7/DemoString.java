a543a6b35832f1d89317039589ab4d13cf3de25a
DHARSHINI R <113581056+dharshinir2@users.noreply.github.com>
2025-05-15 17:08:23 +0530

    Update DemoString.java

--- day7/DemoString.java
+++ day7/DemoString.java
@@ -7,11 +7,17 @@ class DemoString{
 		System.out.println("Indent :"+str.indent(2)); 	
 		System.out.println(str.repeat(2)); //repeat2times
 		System.out.print(str.charAt(2)); //returns in character 
+
+		
 		String a="hello";
 		String b="hello";//a and b are string with same reference
 		String c=new String("hello");
 		String d=new String("hello"); //c and d are string with same obj
 		System.out.println(a.equals(b)); //returns true as a and b points same address
 		System.out.println(c==d); //returns false 
+
+		
+		System.out.println(str.substring(0,4);
+		System.out.print(str.replace('o','q');				   
 	}
 }
